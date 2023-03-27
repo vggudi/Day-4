1c8fe269ff0d256c4cfadad125f620f1e2e16c92
Venkatesh Gudi <vggudi@gmail.com>
2023-03-27 19:51:38 +0530

    Task-4 Commit

--- Question-1.js
+++ Question-1.js
@@ -1,20 +1,22 @@
-let obj1 ={
+var obj1 ={
     "name":"Person1",
     "age":5
-}
+};
 
-let obj2 ={
+var obj2 ={
     "age":5,
     "name":"Person1"
     
-}
-let comparisionString = "Not Equal";
+};
+var comparisionString = "Not Equal";
 if(Object.keys(obj1).length === Object.keys(obj2).length){
     for (key in obj1){
         if(obj1[key] === obj2[key]){
             comparisionString = "Equal";
+            continue;
         } else {
             comparisionString = "Not Equal";
+            break;
         }
     }
 } else {
